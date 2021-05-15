bf71949b0c42dc1654bbfd32035063219581c2cb
MugBelsa <83317358+MugBelsa@users.noreply.github.com>
2021-05-15 08:18:00 -0500

    2nd commit

---
+++ src/main/java/seesion4_Testing/GitTest.java
@@ -0,0 +1,18 @@
+package seesion4_Testing;
+
+public class GitTest {
+	
+	
+	public static void main(String[] args) {
+		
+		GitTest test = new GitTest();
+		test.AB();
+		
+	}
+	
+	public void AB () {
+		
+		System.out.println("Hello");
+	}
+
+}
